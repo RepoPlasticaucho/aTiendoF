4c34d814505a3455b700f81edc003bb2c10d165a
EdisonLopezEC <edisonlopezec@gmail.com>
2024-05-17 12:46:29 -0500

    Sidenav Responsive and Actions

--- src/app/components/navegation-cl/navegation-cl.component.ts
+++ src/app/components/navegation-cl/navegation-cl.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AuthenticationService } from "src/app/services/authentication.service";
 import { Location } from '@angular/common';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navegation-cl',
@@ -13,17 +14,33 @@ import { Location } from '@angular/common';
 export class NavegationClComponent {
 
   transaccionesExpandidas = false;
+  showNavbar = true; // Variable para controlar la visibilidad de la barra de navegación
+
   
+  toggleNavbar() {
+    //Si la pantalla es pequeña, se oculta la barra de navegación
+    this.showNavbar = !this.showNavbar;
+  }
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
+
   constructor(private breakpointObserver: BreakpointObserver,
     private authService: AuthenticationService,
     private location: Location) {}
 
+    closeSidenavIfSmallScreen(drawer: MatSidenav) {
+      // obtén el tamaño de la pantalla
+      const screenWidth = window.innerWidth;
+      // si la pantalla es pequeña, cierra el mat-sidenav
+      if (screenWidth < 768) {
+        drawer.close();
+      }
+    }
   logout() {
     this.authService.logout();
     this.location.replaceState('/');
