14e61cd8f1f69b08612e9c0442f4e20541be9b5b
EdisonLopezEC <edisonlopezec@gmail.com>
2024-05-20 08:49:30 -0500

    Responsibe Dashboard Personal

--- src/app/components/navegation-facturador/navegation-facturador.component.ts
+++ src/app/components/navegation-facturador/navegation-facturador.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AuthenticationService } from "src/app/services/authentication.service";
 import { Location } from '@angular/common';
+import { MatSidenav } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-navegation-facturador',
@@ -13,7 +15,15 @@ import { Location } from '@angular/common';
 export class NavegationFacturadorComponent {
 
   transaccionesExpandidas = false;
-  
+  showNavbar = true; // Variable para controlar la visibilidad de la barra de navegación
+
+
+  toggleNavbar() {
+    //Si la pantalla es pequeña, se oculta la barra de navegación
+    this.showNavbar = !this.showNavbar;
+  }
+
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
@@ -23,6 +33,15 @@ export class NavegationFacturadorComponent {
   constructor(private breakpointObserver: BreakpointObserver,
     private authService: AuthenticationService,
     private location: Location) {}
+
+    closeSidenavIfSmallScreen(drawer: MatSidenav) {
+      // obtén el tamaño de la pantalla
+      const screenWidth = window.innerWidth;
+      // si la pantalla es pequeña, cierra el mat-sidenav
+      if (screenWidth < 768) {
+        drawer.close();
+      }
+    }
 
   logout() {
     this.authService.logout();
